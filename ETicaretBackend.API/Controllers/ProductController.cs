7f979c386433899c467a3d47b9796cf11046bc33
ersel <erseldinckaya1767@hotmail.com>
2023-08-29 14:44:45 +0300

    EF Core Tracking

--- ETicaretBackend.API/Controllers/ProductController.cs
+++ ETicaretBackend.API/Controllers/ProductController.cs
@@ -20,10 +20,14 @@ public class ProductController : ControllerBase
     [HttpGet]
     public async Task Get()
     {
-        await _productWriteRepository.AddRangeAsync(new()
-        {
-            new() { Id = Guid.NewGuid(), CreatedDate = DateTime.UtcNow, Name = "Product1", Price = 100, Stock = 10 }
-        });
+        // await _productWriteRepository.AddRangeAsync(new()
+        // {
+        //     new() { Id = Guid.NewGuid(), CreatedDate = DateTime.UtcNow, Name = "Product1", Price = 100, Stock = 10 }
+        // });
+        // await _productWriteRepository.SaveAsync();
+
+        Product p = await _productReadRepository.GetByIdAsync("dc6fd4d5-c6b4-4c43-b49e-8ac58a2d9ccc", false);
+        p.Name = "Ersel";
         await _productWriteRepository.SaveAsync();
     }
 
